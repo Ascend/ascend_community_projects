1b75345289d6b7e594d26a663216c1e0429fea95
13510421536 <huangheyang@huawei.com>
2022-07-27 15:07:57 +0800

    code check modify

--- MoveitMechanicalArmSimulation/state_note.py
+++ MoveitMechanicalArmSimulation/state_note.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2.7
+# coding=utf-8
 
 """
 # Copyright(C) 2022. Huawei Technologies Co.,Ltd. All rights reserved.
@@ -16,27 +17,29 @@
 # limitations under the License.
 """
 
+
 import datetime
 import threading
 import sys
 import copy
 import signal
+from math import pi
 import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
-from math import pi
+
 from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
 import serial
 
 
 def sigint_handler(signum, frame):
-  signum = signum
-  frame = frame
-  global ISSIGINTUP
-  ISSIGINTUP = True
-  print("catched interrupt signal")
+    signum = signum
+    frame = frame
+    global ISSIGINTUP
+    ISSIGINTUP = True
+    print("catched interrupt signal")
 
 
 signal.signal(signal.SIGINT, sigint_handler)
@@ -46,38 +49,40 @@ ISSIGINTUP = False
 
 
 if __name__ == '__main__':
-  moveit_commander.roscpp_initialize(sys.argv)
-  rospy.init_node('my_move_group_python_interface_tutorial', anonymous=True)
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('my_move_group_python_interface_tutorial', anonymous=True)
 
-  group_name = "panda_arm"
-  move_group = moveit_commander.MoveGroupCommander(group_name)
+    GROUP_NAME = "panda_arm"
+    move_group = moveit_commander.MoveGroupCommander(GROUP_NAME)
 
-  ser = serial.Serial('/dev/ttyAMA0', 3000000, 8, 'N', 1)
-  flag = ser.is_open
+    ser = serial.Serial('/dev/ttyAMA0', 3000000, 8, 'N', 1)
+    flag = ser.is_open
 
-  if flag:
-    print('success')
-  else:
-    print('Open Error')
+    if flag:
+        print('success')
+    else:
+        print('Open Error')
+        exit()
 
-  time_step = 0
-  begin_time = datetime.datetime.now()
-  one_step = 2
+    time_step = 0
+    begin_time = datetime.datetime.now()
+    one_step = 2
 
-  while True:
-    cur_time = (datetime.datetime.now() - begin_time).total_seconds()
-    if cur_time >= (time_step + one_step):
-      time_step = time_step + one_step
-      
-      joint_goal = move_group.get_current_joint_values()
-      print("joint:", joint_goal[0], joint_goal[1], joint_goal[2], joint_goal[3], joint_goal[4], joint_goal[5], joint_goal[6])
-      # ser.write("help".encode('utf-8'))
-      joint_len = len(joint_goal)
-      for i in range(joint_len):
-        tmp_val = joint_goal[i]
-        str = '%.3f'%tmp_val
-        ser.write(str.encode('utf-8'))
-    if ISSIGINTUP:
-      ser.close()
-      print("Exit")
-      break 
+    while True:
+        cur_time = (datetime.datetime.now() - begin_time).total_seconds()
+        if cur_time >= (time_step + one_step):
+            time_step = time_step + one_step
+        
+            joint_goal = move_group.get_current_joint_values()
+            print("joint:", joint_goal[0], joint_goal[1], joint_goal[2], joint_goal[3], joint_goal[4], joint_goal[5], joint_goal[6])
+            # write state to uart dev
+            joint_len = len(joint_goal)
+            for i in range(joint_len):
+                tmp_val = joint_goal[i]
+                str = '%.3f'%tmp_val
+                ser.write(str.encode('utf-8'))
+        if ISSIGINTUP:
+            ser.close()
+            print("Exit")
+            break
+
