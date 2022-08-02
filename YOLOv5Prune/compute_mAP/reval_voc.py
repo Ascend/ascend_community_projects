360e24c3a8160d1e8a9043a1448ca97eae70e1c5
guo_xuanworld <2698345810@qq.com>
2022-08-02 14:59:12 +0800

    update

--- YOLOv5Prune/compute_mAP/reval_voc.py
+++ YOLOv5Prune/compute_mAP/reval_voc.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-"""Reval = re-eval. Re-evaluate saved detections."""
 
 import os
 import sys
