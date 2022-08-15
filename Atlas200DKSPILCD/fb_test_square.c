179fc2763e81dd937b1a4452ff6dca01e63d5b48
lqw <616070323@qq.com>
2022-08-15 08:01:15 +0000

    update Atlas200DKSPILCD/fb_test_square.c.
    Signed-off-by: lqw <616070323@qq.com>

--- Atlas200DKSPILCD/fb_test_square.c
+++ Atlas200DKSPILCD/fb_test_square.c
@@ -24,16 +24,16 @@
 #include <string.h>
 #include <sys/ioctl.h>
 
-struct fb_var_screeninfo vinfo = {0}; // 定义结构体变量，读取屏幕信息时用来记录屏幕可变信息的
-unsigned char *fb_base; // 显存的基地址
-unsigned int line_length;
-unsigned int bytes_per_pixel; // 每像素占用字节数
+struct g_fb_var_screeninfo vinfo = {0}; // 定义结构体变量，读取屏幕信息时用来记录屏幕可变信息的
+unsigned char *g_fb_base; // 显存的基地址
+unsigned int g_line_length;
+unsigned int g_bytes_per_pixel; // 每像素占用字节数
 
 void set_pixel_color(int x, int y, unsigned int color) // 将坐标为(x,y)的像素设为指定颜色
 {
-	int index = y * line_length + x * bytes_per_pixel;
+	int index = y * g_line_length + x * g_bytes_per_pixel;
  
-	*(unsigned int *)(fb_base + index) = color;
+	*(unsigned int *)(g_fb_base + index) = color;
 }
  
 int main(void)
@@ -50,24 +50,24 @@ int main(void)
 		return -1;
 	}
 
-	if (ioctl(fd_fb, FBIOGET_VSCREENINFO, &vinfo)) // 获取可变的参数fb_var_screeninfo的值
+	if (ioctl(fd_fb, FBIOGET_VSCREENINFO, &vinfo)) // 获取可变的参数g_fb_var_screeninfo的值
 	{
 		perror("ioctl");
 		return -1;
 	}
 
-	bytes_per_pixel = vinfo.bits_per_pixel / bits_per_byte;
-	line_length = vinfo.xres_virtual * bytes_per_pixel;
-	frame_buffer_size = vinfo.xres_virtual * vinfo.yres_virtual * bytes_per_pixel;
+	g_bytes_per_pixel = vinfo.bits_per_pixel / bits_per_byte;
+	g_line_length = vinfo.xres_virtual * g_bytes_per_pixel;
+	frame_buffer_size = vinfo.xres_virtual * vinfo.yres_virtual * g_bytes_per_pixel;
 
-	fb_base = (unsigned char *)mmap(NULL, frame_buffer_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd_fb, 0); // 通过mmap获取显存地址
-	if (fb_base == (unsigned char *)-1)
+	g_fb_base = (unsigned char *)mmap(NULL, frame_buffer_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd_fb, 0); // 通过mmap获取显存地址
+	if (g_fb_base == (unsigned char *)-1)
 	{
 		printf("can't mmap\n");
 		return -1;
 	}
 
-	memset(fb_base, 0xff, frame_buffer_size); // 将全屏设为白色
+	memset(g_fb_base, 0xff, frame_buffer_size); // 将全屏设为白色
  
 	for (i = xs; i <= xe; i++)
 	{
@@ -80,7 +80,7 @@ int main(void)
 		set_pixel_color(xe, i, 0xfea0); // 金
 	}
 
-	munmap(fb_base, frame_buffer_size); // 释放显存映射
+	munmap(g_fb_base, frame_buffer_size); // 释放显存映射
 	close(fd_fb);
  
 	return 0;
