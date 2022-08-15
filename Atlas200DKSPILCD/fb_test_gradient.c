78ef49f619f5a10736a2f111e81d29403b7eef69
lqw <616070323@qq.com>
2022-08-15 07:13:40 +0000

    add test
    Signed-off-by: lqw <616070323@qq.com>

---
+++ Atlas200DKSPILCD/fb_test_gradient.c
@@ -0,0 +1,84 @@
+/**
+* Copyright(C) 2022. Huawei Technologies Co.,Ltd. All rights reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <linux/fb.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/ioctl.h>
+
+struct fb_var_screeninfo vinfo = {0}; // 定义结构体变量，读取屏幕信息时用来记录屏幕可变信息的
+unsigned char *fb_base; // 显存的基地址
+unsigned int line_length;
+unsigned int bytes_per_pixel; // 每像素占用字节数
+
+void set_pixel_color(int x, int y, unsigned int color) // 将坐标为(x,y)的像素设为指定颜色
+{
+	int index = y * line_length + x * bytes_per_pixel; 
+	*(unsigned int *)(fb_base + index) = color;
+}
+ 
+int main(void)
+{
+	int i, j;
+	int bits_per_byte = 8;
+	int pixels_per_color = 5;
+	int width = 240;
+	int height = 320;
+	int color_num = 32;
+	int fd_fb;
+	int frame_buffer_size; // 显存大小
+	fd_fb = open("/dev/fb0", O_RDWR); // 打开fb0
+	if (fd_fb < 0)
+	{
+		perror("open");
+		return -1;
+	}
+
+	if (ioctl(fd_fb, FBIOGET_VSCREENINFO, &vinfo)) // 获取可变的参数fb_var_screeninfo的值
+	{
+		perror("ioctl");
+		return -1;
+	}
+
+	bytes_per_pixel = vinfo.bits_per_pixel / bits_per_byte;
+	line_length = vinfo.xres_virtual * bytes_per_pixel;
+	frame_buffer_size = vinfo.xres_virtual * vinfo.yres_virtual * bytes_per_pixel;
+
+	fb_base = (unsigned char *)mmap(NULL, frame_buffer_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd_fb, 0); // 通过mmap获取显存地址
+	if (fb_base == (unsigned char *)-1)
+	{
+		printf("can't mmap\n");
+		return -1;
+	}
+
+	for(j = 0 ; j < height ; j++)
+	{
+		for (i = 0 ; i < width ; i++)
+		{
+			set_pixel_color(i, j, color_num*(j/pixels_per_color));
+		}
+	}
+
+	munmap(fb_base, frame_buffer_size); // 释放显存映射
+	close(fd_fb);
+ 
+	return 0;
+}
---
+++ Atlas200DKSPILCD/test_mpu6050.c
@@ -0,0 +1,82 @@
+/**
+* Copyright(C) 2022. Huawei Technologies Co.,Ltd. All rights reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	short resive_data[6];  // 保存收到的 mpu6050转换结果数据，依次为 AX(x轴角度), AY, AZ 。GX(x轴加速度), GY ,GZ
+	int error;
+	int AX, AY, AZ, GX, GY, GZ;	// 原始数据
+	float AX_zeroset=-0.05, AY_zeroset=0.00, AZ_zeroset=0.19, GX_zeroset=75.93, GY_zeroset=19.12, GZ_zeroset=21.35;	// 调零参数
+	int acc_trans=16384, gyro_trans=16.4;
+	float AX_act, AY_act, AZ_act, GX_act, GY_act, GZ_act;
+	char *buf;
+	int sleep_time = 100000;
+
+	/*打开文件*/
+	int fd = open("/dev/I2C1_mpu6050", O_RDWR);
+	if(fd < 0)
+	{
+		printf("open file : %s failed !\n", argv[0]);
+		return -1;
+	}
+
+	while(1) {
+	/*读取数据*/
+	error = read(fd,resive_data,12);
+	if(error < 0)
+	{
+		printf("write file error! \n");
+		close(fd);
+		/*判断是否关闭成功*/
+	}
+
+	AX = (int)resive_data[0];
+	AY = (int)resive_data[1];
+	AZ = (int)resive_data[2];
+	GX = (int)resive_data[3];
+	GY = (int)resive_data[4];
+	GZ = (int)resive_data[5];
+
+	/*调零 单位转换*/
+	AX_act = (float)(AX)/acc_trans+AX_zeroset;
+	AY_act = (float)(AY)/acc_trans+AY_zeroset;
+	AZ_act = (float)(AZ)/acc_trans+AZ_zeroset;
+	GX_act = ((float)(GX)+GX_zeroset)/gyro_trans;
+	GY_act = ((float)(GY)+GY_zeroset)/gyro_trans;
+	GZ_act = ((float)(GZ)+GZ_zeroset)/gyro_trans;
+
+	/*将解算后的数据显示在LCD上*/
+	sprintf(buf, "bash display_mpu6050.sh %.2f %.2f %.2f %.2f %.2f %.2f", AX_act, AY_act, AZ_act, GX_act, GY_act, GZ_act);
+	system(buf);
+
+	usleep(sleep_time);
+	}
+
+	/*关闭文件*/
+	error = close(fd);
+	if(error < 0)
+	{
+		printf("close file error! \n");
+	}
+
+	return 0;
+}
