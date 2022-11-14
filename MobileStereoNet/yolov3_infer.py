4e9047909dcd227f386b5ba5b3690fd9010306dd
cchenghao <867240157@qq.com>
2022-11-14 21:40:54 +0800

    Add Opencv Process

--- MobileStereoNet/yolov3_infer.py
+++ MobileStereoNet/yolov3_infer.py
@@ -5,37 +5,53 @@ from mindx.sdk.base import Tensor, Model, Size, log, ImageProcessor, post, BTens
 import cv2
 
 device_id = 0  # 芯片ID
-model_path = "./model/yolov3_tf_bs1_fp16.om"  # 模型的路径
+model_path = "./model/yolov3_tf_aipp.om"  # 模型的路径
 config_path = "./model/yolov3_tf_bs1_fp16.cfg"  # 模型配置文件的路径
 label_path = "./model/yolov3.names"  # 分类标签文件的路径
 yolo_resizelen = 416
+b_usedvpp = False  # 使用dvpp图像处理器启用，使用opencv时False
 
 def yolov3(image_path):
     yolov3 = Model(model_path, device_id)  # 创造模型对象
     imageTensor = []
-
+    if b_usedvpp:
     # 创造图像处理器对象!!!!!使用该方法处理后数据已在device侧
-    imageProcessor0 = ImageProcessor(device_id)
-    decodedImg = imageProcessor0.decode(image_path, base.nv12)
-    imageProcessor1 = ImageProcessor(device_id)
-    size_cof = Size(yolo_resizelen, yolo_resizelen)
-    resizeImg = imageProcessor1.resize(decodedImg, size_cof)
+        imageProcessor0 = ImageProcessor(device_id)
+        decodedImg = imageProcessor0.decode(image_path, base.nv12)
+        
+        horigin = decodedImg.original_height
+        worigin = decodedImg.original_width
 
-    imageTensor = [resizeImg.to_tensor()]  # 推理前需要转换为tensor的List，数据已在device侧无需转移
-    print("-----------Img to Tensor Sucess!---------------")
+        imageProcessor1 = ImageProcessor(device_id)
+        size_cof = Size(yolo_resizelen, yolo_resizelen)
+        resizeImg = imageProcessor1.resize(decodedImg, size_cof)
+        imageTensor = [resizeImg.to_tensor()]  # 推理前需要转换为tensor的List，数据已在device侧无需转移
 
+    else:
+        image = np.array(cv2.imread(image_path))
+        size_cof = (yolo_resizelen, yolo_resizelen)
+
+        horigin = image.shape[0]
+        worigin = image.shape[1]
+
+        resizeImg = cv2.resize(image, size_cof, interpolation=cv2.INTER_LINEAR)
+        
+        yuv_img =  cv2.cvtColor(resizeImg, cv2.COLOR_BGR2YUV)
+        yuv_img = yuv_img[np.newaxis, :, :]
+        imageTensor = Tensor(yuv_img) # 推理前需要转换为tensor的List，使用Tensor类来构建。
+        
+        imageTensor.to_device(device_id) # !!!!!重要，需要转移至device侧
+        imageTensor = [imageTensor]
     outputs = yolov3.infer(imageTensor)
     print("-----------YOLO Infer Success!----------------")
     yolov3_post = post.Yolov3PostProcess(
         config_path=config_path, label_path=label_path)  # 构造对应的后处理对象
     
-    
-
     resizeInfo = base.ResizedImageInfo()
     resizeInfo.heightResize = yolo_resizelen
     resizeInfo.widthResize = yolo_resizelen
-    resizeInfo.heightOriginal = decodedImg.original_height
-    resizeInfo.widthOriginal = decodedImg.original_width
+    resizeInfo.heightOriginal = horigin
+    resizeInfo.widthOriginal = worigin
 
     inputs = []
     for i in range(len(outputs)):
@@ -47,5 +63,3 @@ def yolov3(image_path):
     results = yolov3_post.process(inputs, [resizeInfo] * 2)
     
     return results[0]
-
-
